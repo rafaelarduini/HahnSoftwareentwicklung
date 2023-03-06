b790ee846bf41632abb0007fdd5c973ef26287d2
Rafael Arduini Cavalcanti <rafaelarduini@hotmail.com>
2023-03-06 19:21:09 -0300

    Added SqlContext

---
+++ HahnSoftwareentwicklung.Infrastructure/Data/SqlContext.cs
@@ -0,0 +1,30 @@
+﻿using HahnSoftwareentwicklung.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace HahnSoftwareentwicklung.Infrastructure.Data
+{
+    public class SqlContext : DbContext
+    {
+        public SqlContext() { }
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options) { }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("RegistrationDate") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("RegistrationDate").CurrentValue = DateTime.Now;
+                }
+                else if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("RegistrationDate").IsModified = false;
+                }
+            }
+            return base.SaveChanges();
+        }
+    }
+}
