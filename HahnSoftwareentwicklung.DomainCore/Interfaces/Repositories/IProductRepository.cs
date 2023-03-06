ebaaf802a2f11c1ac59d92c5ef8b9fa53d9afe39
Rafael Arduini Cavalcanti <rafaelarduini@hotmail.com>
2023-03-06 19:44:36 -0300

    Added Client and Product interfaces

---
+++ HahnSoftwareentwicklung.DomainCore/Interfaces/Repositories/IProductRepository.cs
@@ -0,0 +1,8 @@
+﻿using HahnSoftwareentwicklung.Domain.Entities;
+
+namespace HahnSoftwareentwicklung.DomainCore.Interfaces.Repositories
+{
+    public interface IProductRepository : IBaseRepository<Product>
+    {
+    }
+}
