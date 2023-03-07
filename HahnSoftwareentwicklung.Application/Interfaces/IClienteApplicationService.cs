8c6f8b7f5a55374b002c6b229a8147d2cfc6d222
Rafael Arduini Cavalcanti <rafaelarduini@hotmail.com>
2023-03-06 21:23:23 -0300

    Added applications services interfaces

---
+++ HahnSoftwareentwicklung.Application/Interfaces/IClienteApplicationService.cs
@@ -0,0 +1,17 @@
+﻿using HahnSoftwareentwicklung.Application.Dtos;
+
+namespace HahnSoftwareentwicklung.Application.Interfaces
+{
+    public interface IClienteApplicationService
+    {
+        void Add(ClientDto clienteDto);
+
+        void Update(ClientDto clienteDto);
+
+        void Delete(ClientDto clienteDto);
+
+        IEnumerable<ClientDto> GetAll();
+
+        ClientDto Get(int id);
+    }
+}
---
+++ HahnSoftwareentwicklung.Application/Interfaces/IProductApplicationService.cs
@@ -0,0 +1,17 @@
+﻿using HahnSoftwareentwicklung.Application.Dtos;
+
+namespace HahnSoftwareentwicklung.Application.Interfaces
+{
+    internal interface IProductApplicationService
+    {
+        void Add(ProductDto productDto);
+
+        void Update(ProductDto productDto);
+
+        void Delete(ProductDto productDto);
+
+        IEnumerable<ProductDto> GetAll();
+
+        ProductDto Get(int id);
+    }
+}
