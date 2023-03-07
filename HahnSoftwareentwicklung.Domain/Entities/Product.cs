6a19861a81d9189e27c150ec45ac8e4afd4d5257
Rafael Arduini Cavalcanti <rafaelarduini@hotmail.com>
2023-03-06 21:18:00 -0300

    Added Product and Client Dtos

--- HahnSoftwareentwicklung.Domain/Entities/Product.cs
+++ HahnSoftwareentwicklung.Domain/Entities/Product.cs
@@ -3,7 +3,7 @@
     public class Product : Base
     {
         public string Name { get; set; }
-        public decimal Valor { get; set; }
+        public decimal Price { get; set; }
         public bool IsAvailable { get; set; }
     }
 }
